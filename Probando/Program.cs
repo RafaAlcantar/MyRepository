e07bce9436674ba43741579054067f055538006a
Rafa Alcantar <rafa.cia.aj.11@gmail.com>
2025-05-17 12:41:49 -0600

    Nueva cadena sensible

--- Probando/Program.cs
+++ Probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World. I am Rafa Alcantar!");
 
-string stripeApiKey = "clave_sensible";
+string stripeApiKey = "sk_test_4eC39HqLyjWDarjtT1";
